7231174f58ac9481c8d36877cd41da21fbf1ce80
Minseong Bae <kyle9103@gmail.com>
2024-11-09 06:16:58 +0900

    Update spice.py

--- spice/spice.py
+++ spice/spice.py
@@ -51,7 +51,7 @@ class Spice:
               "refs" : ref
             })
 
-        cwd = "/kaggle/working"
+        cwd = "/kaggle/working/pycocoevalcap/spice"
         temp_dir=os.path.join(cwd, TEMP_DIR)
         in_file = tempfile.NamedTemporaryFile(delete=False, dir=temp_dir,
                                               mode='w+')
